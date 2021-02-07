a668d84b90f5e7b06ae8a009f124972a0c230713
colormotor <drand48@gmail.com>
2021-02-07 16:57:42 +0000

    more VTK examples

--- py/examples/vtk_silhouette.py
+++ py/examples/vtk_silhouette.py
@@ -21,45 +21,23 @@ reload(clip)
 import polygonsoup.vtk_utils as vtku
 reload(vtku)
 
-model, pos = vtku.load_model('teapot.obj'), vec(0, -0.7, -14.5)
-#model, pos = vtku.load_model('stanford-bunny.obj'), vec(0, -0.08, -0.55)
-contours = vtku.contour_lines(model, [0,1,0], 80)
+np.set_printoptions(suppress=True)
+#model, pos = vtku.load_model('teapot.obj'), vec(0, -1.7, -14.5)
+model, pos = vtku.load_model('stanford-bunny.obj'), vec(0, -0.08, -0.55)
 
 # Viewport rect
 viewport = make_rect(0, 0, 400, 400)
 
-# Camera transformation
 view = (trans_3d(pos) @
         rotx_3d(0.6) @
         roty_3d(np.random.uniform(-1,1)*0.7))
 # Perspective matrix
 proj = perspective(geom.radians(30), rect_aspect(viewport), 0.1)
-# Viewport transformations 3d -> 2d
+
+contours = vtku.silhouette(model, view, feature_angle=False, border_edges=False)
 contours_v = view_3d(contours, view, proj, viewport, clip=True)
 
 clip_contours = True
-if clip_contours:
-    # Hacky clipping procedure
-    # It assumes contour lines are generated for Y axis, and a viewing angle from the top
-    centroids = [np.mean(z, axis=0) for z in contours]
-    ord = [c[1] for c in centroids]
-    I = np.argsort(ord)
-    contours_v = [contours_v[i] for i in I]
-
-    def clip_sorted(contours):
-        res = []
-        all = []
-        for P in contours[::-1]:
-            if all:
-                Pc = clip.difference(P, all, False, True)
-                all = clip.union(all, P)
-            else:
-                Pc = [P]
-                all = [P]
-            res += Pc
-        return res
-
-    contours_v = clip_sorted(contours_v)
 
 plot.figure(3, 3)
 plot.stroke_rect(viewport, 'r', linestyle=':')
@@ -67,4 +45,4 @@ plot.stroke(contours_v, 'k')
 # Use this to visualize ordering with filled polygons
 # for i, ctr in enumerate(contours_v):
 #     plot.fill_stroke(ctr, np.ones(3)*0.5, 'k', zorder=i)
-plot.show(title='Teapot')
+plot.show(title='Silhouette')
---
+++ py/polygonsoup/vtk_utils.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# Utilities to interface with VTK
+import numpy as np
+import polygonsoup.geom as geom
+import vtk
+vec = geom.vec
+
+def load_model(path):
+    '''Load a model'''
+    model = vtk.vtkOBJReader()
+    model.SetFileName(path)
+    model.Update()
+    return model.GetOutput()
+
+
+def contour_lines(poly_data, normal, num=40):
+    ''' Generates contour lines for a polydata object'''
+    normals = vtk.vtkPolyDataNormals()
+    normals.SetInputData(poly_data)
+    normals.Update()
+
+    center = poly_data.GetCenter()
+    bounds = poly_data.GetBounds()
+    r = geom.distance(vec(bounds[0], bounds[2], bounds[4]), center)
+
+    plane = vtk.vtkPlane()
+    plane.SetOrigin(center)
+    plane.SetNormal(normal)
+
+    cutter = vtk.vtkCutter()
+
+    cutter.SetCutFunction(plane)
+    cutter.SetInputData(normals.GetOutput())
+    cutter.GenerateCutScalarsOn()
+    cutter.GenerateValues(num, -r, r)
+    cutter.Update()
+
+    return vtk_to_polylines(cutter)
+
+def silhouette(poly_data, view, feature_angle=True, border_edges=True):
+    ''' Generates silhouette contours for a polydata object'''
+    # view matrix to Vtk camera
+    campos = -view.T[:3,:3]@view[:3,3]
+    camz = -view.T[:3,2]
+    camera = vtk.vtkCamera()
+    camera.SetPosition(campos)
+    camera.SetFocalPoint(campos + camz)
+    # debug
+    # viewd = geom.vtk_to_np_matrix(camera.GetViewTransformMatrix())
+    # print(view-viewd)
+
+    silhouette = vtk.vtkPolyDataSilhouette()
+    silhouette.SetInputData(poly_data)
+    silhouette.SetCamera(camera)
+    silhouette.SetEnableFeatureAngle(feature_angle)
+    silhouette.SetBorderEdges(border_edges)
+    silhouette.Update()
+    contours = vtk_to_polylines(silhouette)
+    return contours
+
+def vtk_to_polylines(obj):
+    '''Convert vtk output to polylines'''
+    strips = vtk.vtkStripper()
+    strips.SetInputData(obj.GetOutput())
+    strips.Update()
+
+    poly = strips.GetOutput()
+    pts = np.array(poly.GetPoints().GetData())
+    lines = np.array(poly.GetLines().GetData())
+    n = len(lines)
+    res = []
+    i = 0
+    while i < n:
+        res.append([])
+        nl = lines[i]
+        i += 1
+        for c in range(nl):
+            res[-1].append(pts[lines[i]])
+            i += 1
+
+    return res
+
+def vtk_to_ndarray(vmatrix):
+    """Return vtkMatrix4x4 or vtkMatrix3x3 elements as numpy array.
+    see https://discourse.slicer.org/t/vtk-transform-matrix-as-python-list-tuple-array/11797/2
+    """
+    from vtk import vtkMatrix4x4
+    from vtk import vtkMatrix3x3
+    if isinstance(vmatrix, vtkMatrix4x4):
+        sz = 4
+    elif isinstance(vmatrix, vtkMatrix3x3):
+        sz = 3
+    else:
+        raise RuntimeError("Input must be vtk.vtkMatrix3x3 or vtk.vtkMatrix4x4")
+    narray = np.eye(sz)
+    vmatrix.DeepCopy(narray.ravel(), vmatrix)
+    return narray
