0ae52fc5d456bb9f0bae7a9d8ed022a53b8fe13c
colormotor <drand48@gmail.com>
2022-02-18 15:45:03 +0000

    plotter

--- py/examples/cubes.py
+++ py/examples/cubes.py
@@ -44,7 +44,7 @@ plotter = plotters.AxiDrawClient() # Socket connection to axidraw_server.py
 #plotter = plotters.AxiPlotter() # Direct connection to AxiDraw using axi module
 #plotter = plotters.NoPlotter() # Simply draws output
 
-plot.figure('A5')
+plot.figure('A5', plotter=plotter)
 plot.stroke_rect(viewport, 'r', linestyle=':')
 plot.stroke(Sv, 'k')
 plot.show(title='Cubes')
