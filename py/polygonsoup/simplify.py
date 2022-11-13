5a35b3755f36438f5a90472d31d259c93da9a9ba
colormotor <drand48@gmail.com>
2022-11-13 21:23:03 +0100

    vs simplification test

--- py/polygonsoup/simplify.py
+++ py/polygonsoup/simplify.py
@@ -113,3 +113,83 @@ def dp_simplify(M, eps, get_indices=False, closed=False, dist=pldist):
     if get_indices:
         return M[I], I
     return M[I]
+
+
+def vw_simplify(P, tol, closed=False, get_indices=False):
+    """ Visvalingam-Whyatt simplification
+    from https://github.com/hrishioa/Aviato/blob/bafb2ca8c3d3f11596398e57198a3d62e9a2d39d/app/kartograph/simplify/visvalingam.py
+    implementation borrowed from @migurski:
+    https://github.com/migurski/Bloch/blob/master/Bloch/__init__.py#L133
+    """
+    if closed:
+        P = np.vstack([P, P[0]])
+
+    if len(P) < 3:
+        return P
+
+    min_area = tol ** 2
+
+    I = list(range(len(P)))  # pts stores an index of all non-deleted points
+
+    while len(I) > 4:
+        preserved, popped = set(), []
+        areas = []
+
+        for i in range(1, len(I) - 1):
+            x1, y1 = P[I[i - 1]]
+            x2, y2 = P[I[i]]
+            x3, y3 = P[I[i + 1]]
+            # compute and store triangle area
+            areas.append((tri_area(x1, y1, x2, y2, x3, y3), i))
+
+        areas = sorted(areas)
+
+        if not areas or areas[0][0] > min_area:
+            # there's nothing to be done
+            break
+
+        # Reduce any segments that makes a triangle whose area is below
+        # the minimum threshold, starting with the smallest and working up.
+        # Mark segments to be preserved until the next iteration.
+
+        for (area, i) in areas:
+
+            if area > min_area:
+                # there won't be any more points to remove.
+                break
+
+            if i - 1 in preserved or i + 1 in preserved:
+                # the current segment is too close to a previously-preserved one.
+                #print "-pre", preserved
+                continue
+
+            popped.append(i)
+
+            # make sure that the adjacent points
+            preserved.add(i - 1)
+            preserved.add(i + 1)
+
+        if len(popped) == 0:
+            # no points removed, so break out of loop
+            break
+
+        popped = sorted(popped, reverse=True)
+        for i in popped:
+            # remove point from index list
+            I = I[:i] + I[i + 1:]
+
+    Q = P[I]
+    if closed:
+        Q = Q[:-1]
+        I = I[:-1]
+    if get_indices:
+        return Q, I
+    return Q
+
+def tri_area(x1, y1, x2, y2, x3, y3):
+    """
+    computes the area of a triangle given by three points
+    implementation taken from:
+    http://www.btinternet.com/~se16/hgb/triangle.htm
+    """
+    return abs((x2*y1-x1*y2)+(x3*y2-x2*y3)+(x1*y3-x3*y1))/2.0
