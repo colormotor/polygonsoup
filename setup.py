029d6a67469e39277319a1b77586c103bf29a359
colormotor <drand48@gmail.com>
2024-07-09 10:03:39 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import sys
 
 setup(name='polygonsoup',
-        version='0.1',
+        version='0.2',
         description='Python plotter utilities',
         url='',
         author='Daniel Berio',
